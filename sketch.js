c7cabf2707586c0a0723f44b1284bbc7b86ab233
matheuseduardo7 <148362118+matheuseduardo7@users.noreply.github.com>
2023-11-01 11:43:40 -0300

    adiconndo arquivos html,css e js

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(400, 400);
+    background("lime");
+  }
+  
+  function draw() {
+  stroke ("black")
+    fill("blue");
+    
+    
+    
+    if(mouseIsPressed) {
+       rect(mouseX, mouseY, 20, 20);
+    }
+  }function setup() {
+    createCanvas(400, 400);
+    background("lime");
+  }
+  
+  function draw() {
+  stroke ("black")
+    fill("blue");
+    
+    
+    
+    if(mouseIsPressed) {
+       rect(mouseX, mouseY, 20, 20);
+    }
+  }
+    
